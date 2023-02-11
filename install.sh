b924c8b4df84e93c4804432a483f898d82f0e67c
Hikiso <89852333+MahanSafari@users.noreply.github.com>
2023-02-11 16:05:01 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -107,7 +107,7 @@ install_x-ui() {
     cd /usr/local/
 
     if [ $# == 0 ]; then
-        last_version=$(curl -Ls "https://api.github.com/repos/MahanSafari/zerostore-x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        last_version=$(curl -Ls "https://api.github.com/repos/hossinasaadi/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$last_version" ]]; then
             echo -e "${red}refresh x-ui version failed,it may due to Github API restriction,please try it later${plain}"
             exit 1
